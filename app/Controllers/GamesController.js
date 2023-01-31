70eff7f70692f566e7f7c9aa29cb44ea44911e92
Parker <ward.parker26@gmail.com>
2023-01-30 17:27:30 -0700

    first commit

---
+++ app/Controllers/GamesController.js
@@ -0,0 +1,18 @@
+import { appState } from "../AppState";
+
+export class GamesController{
+    drawplayers(){
+        let players = appState.players
+        console.log('drawing players', players);
+    }
+
+}
+
+
+
+
+
+
+
+
+
